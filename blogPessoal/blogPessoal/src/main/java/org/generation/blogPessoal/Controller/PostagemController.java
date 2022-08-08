cca6df355cc137f7f84ceb7c92e9fd265091d79a
Barbara Ayres <ayresbarbara2@gmail.com>
2022-08-08 10:53:46 -0300

    Blog Pessoal

--- blogPessoal/blogPessoal/src/main/java/org/generation/blogPessoal/Controller/PostagemController.java
+++ blogPessoal/blogPessoal/src/main/java/org/generation/blogPessoal/Controller/PostagemController.java
@@ -1,6 +1,7 @@
 package org.generation.blogPessoal.Controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.generation.blogPessoal.Model.Postagem;
 import org.generation.blogPessoal.repository.PostagemRepository;
@@ -8,12 +9,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping ("/postagens")
@@ -41,8 +46,18 @@ public class PostagemController {
 	}
 	@PostMapping
 	public ResponseEntity <Postagem> post (@RequestBody Postagem postagem){ //RequestBody pega o que tem no corpo da requisição
-		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(postagem));
-		
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(postagem));	
 	}
-
+	@PutMapping
+	public ResponseEntity <Postagem> put (@RequestBody Postagem postagem){ 
+		return ResponseEntity.status(HttpStatus.OK).body(repository.save(postagem));
+	}
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@DeleteMapping ("/{id}")
+	public void delete (@PathVariable Long id) {
+	Optional<Postagem> postagem = repository.findById(id);
+		if(postagem.isEmpty())
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+	repository.deleteById(id);
+	}
 }
