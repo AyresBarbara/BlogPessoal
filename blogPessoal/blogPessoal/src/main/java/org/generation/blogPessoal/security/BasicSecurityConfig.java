a504b868f65854c0e76ce28ba677d51aaac9de96
Barbara Ayres <ayresbarbara2@gmail.com>
2022-08-15 23:10:34 -0300

    Blog Pessoal

--- blogPessoal/blogPessoal/src/main/java/org/generation/blogPessoal/security/BasicSecurityConfig.java
+++ blogPessoal/blogPessoal/src/main/java/org/generation/blogPessoal/security/BasicSecurityConfig.java
@@ -25,23 +25,23 @@ public class BasicSecurityConfig {
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
     }
-    
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http
-            .sessionManagement()
-            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-            .and().csrf().disable()
-            .cors();
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().csrf().disable()
+                .cors();
 
         http
-            .authorizeHttpRequests((auth) -> auth
-                .antMatchers("/usuarios/logar").permitAll()
-                .antMatchers("/usuarios/cadastrar").permitAll()
-                .antMatchers(HttpMethod.OPTIONS).permitAll()
-                .anyRequest().authenticated())
-            .httpBasic();
+                .authorizeHttpRequests((auth) -> auth
+                        .antMatchers("/usuarios/logar").permitAll()
+                        .antMatchers("/usuarios/cadastrar").permitAll()
+                        .antMatchers(HttpMethod.OPTIONS).permitAll()
+                        .anyRequest().authenticated())
+                .httpBasic();
 
         return http.build();
 
